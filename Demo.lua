eb74830a344051b7304af92d92ea40399eb2ef4b
Xola Dev <ngiveros@gmail.com>
2026-02-01 20:16:56 +0700

    Update Demo.lua

--- Demo.lua
+++ Demo.lua
@@ -1,35 +1,37 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xoladev/ArchivableLib/refs/heads/main/lib.lua"))()
 
-function menuv()
-	local myMenu = _G.ALib:CreateMenu("LibDemo", "Archivable Library Demo", "DarkTheme")
-	local ElementsTab = _G.ALib:AddTab(myMenu, "Elements", _G.ALib.Assets.dogLOLImage)
-	local SettingsTab = _G.ALib:AddTab(myMenu, "Settings", _G.ALib.Assets.catLOLImage)
+local ALib = _G.ALib
+
+function MenuFunc()
+	local myMenu = ALib:CreateMenu("LibDemo", "Archivable Library Demo", "DarkTheme")
+	local ElementsTab = ALib:AddTab(myMenu, "Elements", ALib.Assets.dogLOLImage)
+	local SettingsTab = ALib:AddTab(myMenu, "Settings", ALib.Assets.catLOLImage)
 
 	local function toggleMenu()
 		myMenu.Main.Visible = not myMenu.Main.Visible
 	end
 	
 	local function buttonClick()
-		_G.ALib:CreateNotification("Button clicked!", "", 1.5, _G.ALib.Assets.catLOLImage)
+		ALib:CreateNotification("Button clicked!", "", 1.5, ALib.Assets.catLOLImage)
 	end
 	local function sliderClick(state)
-		_G.ALib:CreateNotification("Slider Clicked!", "Current State: " .. tostring(state), 1.5, _G.ALib.Assets.catLOLImage)
+		ALib:CreateNotification("Slider Clicked!", "Current State: " .. tostring(state), 1.5, ALib.Assets.catLOLImage)
 	end
 	local function InputValueEdited(text)
-		_G.ALib:CreateNotification("New text!", text, 1.5, _G.ALib.Assets.catLOLImage)
+		ALib:CreateNotification("New text!", text, 1.5, ALib.Assets.catLOLImage)
 	end
 
-	_G.ALib:AddTextLabel(ElementsTab, "Text Label", "Description")
-	_G.ALib:AddSeparator(ElementsTab, "Separator")
-	_G.ALib:AddButton(ElementsTab, "Button", "Description", buttonClick)
-	_G.ALib:AddSlider(ElementsTab, "Slider", "Description", sliderClick)
-	_G.ALib:AddInputValue(ElementsTab, "Input Value", "Description", "PlaceHolder", InputValueEdited)
+	ALib:AddTextLabel(ElementsTab, "Text Label", "Description")
+	ALib:AddSeparator(ElementsTab, "Separator")
+	ALib:AddButton(ElementsTab, "Button", "Description", buttonClick)
+	ALib:AddSlider(ElementsTab, "Slider", "Description", sliderClick)
+	ALib:AddInputValue(ElementsTab, "Input Value", "Description", "PlaceHolder", InputValueEdited)
 	
-	_G.ALib:CreateFloatingButton("DemoToggleMenu", _G.ALib.Assets.catLOLImage, 999, toggleMenu)
+	ALib:CreateFloatingButton("DemoToggleMenu", ALib.Assets.catLOLImage, 999, toggleMenu)
 
-	_G.ALib:AddButton(SettingsTab, "Unload Library", "Deletes GUIs.", function()
-		_G.ALib:DestroyLib()
+	ALib:AddButton(SettingsTab, "Unload Library", "Deletes GUIs.", function()
+		ALib:DestroyLib()
 	end)
 end
 
-_G.ALib:CreateKeySystem("", "Its working!", "Archivable Library Demo (Press 'CheckKey')", "WhiteTheme", menuv)
+ALib:CreateKeySystem("", "Its working!", "Archivable Library Demo (Press 'CheckKey')", "WhiteTheme", MenuFunc)
